4d9b8f2a3f3f34dbb29c22a59ac8dbca66459bd6
Carlos DaLomba <carlos@cometpeakgames.net>
2022-12-20 01:55:33 -0500

    Added first functioning C++ program with CMake!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pixel Art Game
 This is a fun passion/learning project, based on [TheCherno's](https://www.youtube.com/c/TheChernoProject) [Game Programming Series](https://www.youtube.com/watch?v=GFYT7Lqt1h8&list=PLlrATfBNZ98eOOCk2fOFg7Qg5yoQfFAdf&index=1).
+
+This project is in C++ using CMake, Ninja, and bash, despite the original tutorial series being in Java. This is a great way for me to learn C++ while having a goal in mind (creating a simple pixel art game)!
+
+First, I need to learn how to setup the project with GLFW/OpenGL, then start to draw pixels to the screen.
---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This is a helper script to build when Visual Studio is not available
+#       (For example, when developing on MacOS or Linux systems).
+# The default configuration is x64-release, but you may pass a different config as an argument.
+# To see the available values, see the "name" values of the configurationPresets array in CMakePresets.json
+
+# COMMAND LINE USAGE:
+# ./build.sh [CONFIG]
+
+# EXAMPLES:
+#       ./build.sh x64-release
+#       ./build.sh x64-debug
+
+defaultConfig="x64-release"
+
+argCount=$#
+args=("$@")
+
+if [ $argCount -gt 0 ]; then
+    config=${args[0]}
+else
+    config="$defaultConfig"
+fi
+
+printf "Building $config...\n\n"
+printf "Running CMake...\n"
+# cmake  ..
+cmake --preset "$config"
+
+exitCode=$?
+if [ $exitCode -ne 0 ]; then
+    exit $exitCode
+fi
+printf "\n"
+
+printf "Running CMake build system...\n"
+# cmake --build .
+cmake --build out/build/"$config"
+exitCode=$?
+if [ $exitCode -ne 0 ]; then
+    exit $exitCode
+fi
