2e2f4f45eba5f7d0a278c687cb6626611418b834
Carlos DaLomba <carlos@cometpeakgames.net>
2022-12-23 00:11:52 -0500

    SUCCESS! Built and linked with GLFW!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <thread>
+
+#include <GLFW/glfw3.h>
 #include "GameVersion.h"
 
+using namespace std;
+
 int main() {
-    printf(PROJECT_NAME " v" PROJECT_VERSION);
+    printf(PROJECT_NAME " v" PROJECT_VERSION "\n");
+
+    if (glfwInit() == GLFW_FALSE) {
+        printf("GLFW initialization failed!");
+        return 1;
+    }
+
+    printf("Starting...\n");
+    this_thread::sleep_for(chrono::milliseconds(3000));
+    printf("Done testing!\n");
+
+    glfwTerminate();
     return 0;
 }
