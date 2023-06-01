896301bf7ce7787ae3ae5a90515556d0c0ca7e6d
Carlos DaLomba <carlos@cometpeakgames.net>
2023-06-01 00:58:55 -0400

    Extracted the project name from bash script variables only! READY FOR GIT SUBMODULE!

--- scripts/run-debug.sh
+++ scripts/run-debug.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 source $(dirname $0)"/get-platform-default-config.sh"
+source $(dirname $0)"/get-project-name.sh"
 defaultDebugConfig=${defaultConfig/release/debug}
 
 case "$simpleOSName" in
-    "Windows")      ./out/build/$defaultDebugConfig/PixelArtGame.exe;;
-    "MacOS")        ./out/build/$defaultDebugConfig/PixelArtGame;;
-    "Linux")        ./out/build/$defaultDebugConfig/PixelArtGame;;
+    "Windows")      ./out/build/$defaultDebugConfig/$projectName.exe;;
+    "MacOS")        ./out/build/$defaultDebugConfig/$projectName;;
+    "Linux")        ./out/build/$defaultDebugConfig/$projectName;;
 esac
--- scripts/run-release.sh
+++ scripts/run-release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 source $(dirname $0)"/get-platform-default-config.sh"
+source $(dirname $0)"/get-project-name.sh"
 
 case "$simpleOSName" in
-    "Windows")      ./out/build/$defaultConfig/PixelArtGame.exe;;
-    "MacOS")        ./out/build/$defaultConfig/PixelArtGame;;
-    "Linux")        ./out/build/$defaultConfig/PixelArtGame;;
+    "Windows")      ./out/build/$defaultConfig/$projectName.exe;;
+    "MacOS")        ./out/build/$defaultConfig/$projectName;;
+    "Linux")        ./out/build/$defaultConfig/$projectName;;
 esac
